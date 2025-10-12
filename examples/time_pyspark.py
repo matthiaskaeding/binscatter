c5ad7ce9943836abf646ba6807010b7803bd745a
matthiaskaeding <kaedingmatthias@gmail.com>
2025-10-12 23:32:52 +0200

    Update time_pyspark.py

--- examples/time_pyspark.py
+++ examples/time_pyspark.py
@@ -7,14 +7,17 @@ from binscatter import binscatter
 import time
 from math import ceil
 
-N = 1000_000_000
+N = 100_000_000
 CHUNK_SIZE = 1000_000
 N_chunks = ceil(N / CHUNK_SIZE)
 spark = SparkSession.builder.getOrCreate()
 
 with tempfile.TemporaryDirectory() as dirname:
     dir = Path(dirname)
+    print("Writing test files to chunks")
     for i in range(N_chunks):
+        if i % 50 == 0:
+            print(f"Chunk {i} / {N_chunks} ")
         x = np.random.normal(0, 1, CHUNK_SIZE)
         y = np.random.normal(0, 1, CHUNK_SIZE)
         df = pl.DataFrame({"x": x, "y": y})
@@ -24,6 +27,7 @@ with tempfile.TemporaryDirectory() as dirname:
     df = spark.read.parquet(dirname)
     print(df)
 
+    print("Making binscatter - timing starts here")
     t0 = time.perf_counter()
     p = binscatter(df, "x", "y")
     dt = time.perf_counter() - t0
